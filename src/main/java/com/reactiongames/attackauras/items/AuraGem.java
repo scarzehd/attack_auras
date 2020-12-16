20c8be372621ea4724bcb5a655784fbea02bcff8
Ezra Balden <ezrasgamingemail@gmail.com>
2020-12-16 16:57:41 -0500

    asfadsf

---
+++ src/main/java/com/reactiongames/attackauras/items/AuraGem.java
@@ -0,0 +1,10 @@
+package com.reactiongames.attackauras.items;
+
+import com.reactiongames.attackauras.AttackAuras;
+import net.minecraft.item.Item;
+
+public class AuraGem extends Item {
+    public AuraGem() {
+        super(new Item.Properties().group(AttackAuras.TAB));
+    }
+}
